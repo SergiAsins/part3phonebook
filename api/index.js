3a8fb58b9f22971ff283d409b8e35281bd080a47
SergiAsins <sergi.prof@hotmail.com>
2024-07-25 12:03:00 +0200

    more url changes

--- api/index.js
+++ api/index.js
@@ -115,10 +115,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist'), 'index.html')
 }) 
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
