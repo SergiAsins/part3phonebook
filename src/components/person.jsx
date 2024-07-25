d12ff1fcf200ed71a73fdfbd79117f4864c86c03
SergiAsins <sergi.prof@hotmail.com>
2024-07-25 16:33:14 +0200

    Update person.jsx

--- src/components/person.jsx
+++ src/components/person.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Person = ({ person, deletePerson }) => (
+//handleDelete aavns era personDelete
+const Person = ({ person, handleDelete }) => (
   <li>
    {person.name} {person.number}
-  <button onClick={() => deletePerson(person.id)}> delete </button>
+  <button onClick={() => handleDelete(person.id)}> delete </button>
   </li>
 )
 
