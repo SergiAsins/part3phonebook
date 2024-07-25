91b62bfedd5e33a76571ae55620fddcc81eeddde
SergiAsins <sergi.prof@hotmail.com>
2024-07-25 09:55:19 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 // Use environment varibales
 const MONGODB_URI = process.env.MONGODB_URI
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
+//console.log('MONGODB_URI:', process.env.MONGODB_URI);
 if (!MONGODB_URI) {
     console.error('Error: La variable de entorno MONGODB_URI no está definida');
     process.exit(1);
